4b1397551737c2c1e49ed93eaa1b275d4125c882
Muthamizhan <muthamizhanboobalan@gmail.com>
2024-12-03 18:34:25 +0530

    new page

--- Resource/Script/redirect.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const loggedInUserId = localStorage.getItem('loggedInUserId');
-    if (loggedInUserId) {
-        window.location.href = "resource/pages/home.html";
-    }
-});
